84f25f0bd80ae7011cf06100b909b4f24f6e1484
Drew Igoe <drewigoe@gmail.com>
2024-12-04 20:50:45 -0700

    Initial Commit

---
+++ Carbon Footprint Calculator/Carbon_Footprint_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Carbon_Footprint_CalculatorApp.swift
+//  Carbon Footprint Calculator
+//
+//  Created by Drew Igoe on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Carbon_Footprint_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
